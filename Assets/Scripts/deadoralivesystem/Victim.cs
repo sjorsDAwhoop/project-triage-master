30b6a4ffdb273fd65299f9645dd5d7f49c0ec555
sjorsDAwhoop <sjorsvanderkleij2001@gmail.com>
2020-09-03 13:26:30 +0200

    commit

---
+++ Assets/Scripts/deadoralivesystem/Victim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victim
+{
+    
+    public int timeleft;
+    public GameObject body;
+
+    public Victim( int newTimeLeft, GameObject newbody)
+    {
+        
+        timeleft = newTimeLeft;
+        body = newbody;
+        
+    }
+
+
+}
---
+++ Assets/Scripts/deadoralivesystem/bodycounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bodycounter : MonoBehaviour
+{
+    public float human1TimeLeft = 10;
+    public bool timerIsRunning = false;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (human1TimeLeft > 0)
+            {
+                human1TimeLeft -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                human1TimeLeft = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+}
