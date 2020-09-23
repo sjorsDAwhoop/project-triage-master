b1d04f2536b042d6f274b5a43bf9aa35a3c237d6
sjorsDAwhoop <sjorsvanderkleij2001@gmail.com>
2020-09-23 15:52:16 +0200

    Revert "22-09 commit"
    This reverts commit 78cf500a45e17b0f13990d6eb0b9a39f412f44f2.

--- Assets/Scripts/deadoralivesystem/bodycounter.cs
+++ Assets/Scripts/deadoralivesystem/bodycounter.cs
@@ -3,25 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class BodyCounter : MonoBehaviour
+public class bodycounter : MonoBehaviour
 {
-
     public List<float> _timeleft = new List<float>();
     public List<DeathTimer> _timers = new List<DeathTimer>();
-    private Test test;
+    public GameObject cube;
 
-    
    
     public void Start()
     {
-        
         _timeleft.Add(5f);
         _timeleft.Add(1f);
         _timeleft.Add(10f);
-        test = gameObject.GetComponent<Test>();
 
 
-        _timers.Add(new DeathTimer(_timeleft[0], () => test.DeleteObject()));
+        _timers.Add(new DeathTimer(_timeleft[0], () =>  ));
         _timers.Add(new DeathTimer(_timeleft[1], () => Debug.Log("Timer two finished.")));
         _timers.Add(new DeathTimer(_timeleft[2], () => Debug.Log("Timer three finished")));
 
@@ -30,6 +26,7 @@ public class BodyCounter : MonoBehaviour
        
     }
 
+    
     public void Update()
     {
 
