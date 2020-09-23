4df4ef87141ccc66c7c0e120a5452c1e7aaf25f7
sjorsDAwhoop <sjorsvanderkleij2001@gmail.com>
2020-09-23 15:49:17 +0200

    Revert "23-9 commit"
    This reverts commit 233370d0e1a98c6533f6d9ddbc4092a8595f2e9e.

---
+++ Assets/Scripts/deadoralivesystem/DeathTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+using System;
+
+public class DeathTimer : IDisposable
+{
+    public bool Finished = false;
+    public bool Started = false;
+    public  Timer _timer;
+    public DeathTimer(float time, Action actionToBeExecuted)
+    {
+        _timer = new Timer
+        {
+            Interval = time * 1000, // in milliseconds
+            AutoReset = false
+        };
+        _timer.Elapsed += (o, args) =>
+        {
+            actionToBeExecuted();
+            Finished = true;
+        };
+    }
+    public void Start()
+    {
+        Started = true;
+        _timer.Start();
+    }
+    public void Stop()
+    {
+        _timer.Stop();
+    }
+    public void Dispose()
+    {
+        _timer.Dispose();
+    }
+}
---
+++ Assets/Scripts/deadoralivesystem/bodycounter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BodyCounter : MonoBehaviour
+{
+
+    public List<float> _timeleft = new List<float>();
+    public List<DeathTimer> _timers = new List<DeathTimer>();
+    private Test test;
+
+    
+   
+    public void Start()
+    {
+        
+        _timeleft.Add(5f);
+        _timeleft.Add(1f);
+        _timeleft.Add(10f);
+        test = gameObject.GetComponent<Test>();
+
+
+        _timers.Add(new DeathTimer(_timeleft[0], () => test.DeleteObject()));
+        _timers.Add(new DeathTimer(_timeleft[1], () => Debug.Log("Timer two finished.")));
+        _timers.Add(new DeathTimer(_timeleft[2], () => Debug.Log("Timer three finished")));
+
+
+        
+       
+    }
+
+    public void Update()
+    {
+
+        if (_timers.Count > 0)
+        {
+            foreach (var timer in _timers.Where(a => !a.Started))
+            {
+                timer.Start();
+            }
+
+            foreach (var timer in _timers.Where(a => a.Finished))
+            {
+                timer.Dispose();
+            }
+            _timers.RemoveAll(a => a.Finished);
+        }
+
+    }
+
+}
