233370d0e1a98c6533f6d9ddbc4092a8595f2e9e
sjorsDAwhoop <sjorsvanderkleij2001@gmail.com>
2020-09-23 14:42:48 +0200

    23-9 commit

---
+++ Assets/Scripts/deadoralivesystem/AnimationHandeler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandeler : MonoBehaviour
+{   
+    public void aaaaaaaa()
+    {
+        Destroy(gameObject);
+    }       
+}
