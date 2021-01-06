02db18f95c2af6f25d9a7af4da61523314d9b036
sjorsvdkleij <sjorsvanderkleij2001@gmail.com>
2021-01-06 13:00:56 +0100

    commit

---
+++ Assets/Scripts/MedicalEquipment/onattach.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onattach : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Function("hi there");
+    }
+
+    // Update is called once per frame
+    public void Function(string sayit)
+    {
+        Debug.Log(sayit);
+    }
+
+}
